36c127abeca5a5a6bedf067a88456594eca565fa
Scruffysnake <dege.gabriel@gmail.com>
2025-03-11 18:45:11 +0000

    Setup and grid rendering

---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using Raylib_cs;
+using ImGuiNET;
+using rlImGui_cs;
+using System.Numerics;
+
+/*
+    Naming convensions:
+    
+    Constants           :   UPPERCASE_SNAKE_CASE
+    Global variables    :   PascalCase
+    Local variables     :   camelCase
+*/
+
+namespace Simple_Graphing_Calculator;
+class Program
+{
+    const int RESOLUTION_X = 1920;
+    const int RESOLUTION_Y = 1080;
+    const float DEFAULT_ZOOM = 1080 / 10;
+
+    static Vector2 Offset = new Vector2(RESOLUTION_X / 2, RESOLUTION_Y / 2);
+
+    public static void Main()
+    {
+        // Init
+        Raylib.InitWindow(RESOLUTION_X, RESOLUTION_Y, "Simple Graphing Calculator");
+        Raylib.SetTargetFPS(144);
+        rlImGui.Setup();
+
+        // Camera
+        Vector2 targetDefault = new Vector2(0, 0);
+        Camera2D view = new Camera2D(Offset, targetDefault, 0, DEFAULT_ZOOM);
+        Vector2 cameraDefaultPosition = new Vector2(60, 60);
+        Vector2 cameraDefaultSize = new Vector2(300, 80);
+
+        // Main Loop
+        while (!Raylib.WindowShouldClose())
+        {
+            // Input
+            Input(ref view);
+
+            // Draw
+            Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+                DrawAxes(ref view);
+
+                // ImGui
+                rlImGui.Begin();
+
+                    // Camera
+                    ImGui.SetNextWindowPos(cameraDefaultPosition, ImGuiCond.FirstUseEver);
+                    ImGui.SetNextWindowSize(cameraDefaultSize, ImGuiCond.FirstUseEver);
+                    ImGui.Begin("Camera");
+                        ImGui.InputFloat2("Position", ref view.Target);
+                        ImGui.InputFloat("Zoom", ref view.Zoom);
+                    ImGui.End();
+
+                    // Functions
+                    ImGui.SetNextWindowPos(new Vector2(60, 160), ImGuiCond.FirstUseEver);
+                    ImGui.Begin("Functions");
+                        ImGui.Text("This is some text");
+                    ImGui.End();
+                rlImGui.End();
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+    }
+    static void Input(ref Camera2D view)
+    {
+        // Zoom
+        const float ZOOM_INCREMENT = 5;
+        view.Zoom += Raylib.GetMouseWheelMove() * ZOOM_INCREMENT;
+        if (view.Zoom < 0) view.Zoom = 1;
+
+        // Move
+        if (Raylib.IsMouseButtonDown(MouseButton.Right))
+        {
+            Vector2 mouseDelta = Raylib.GetMouseDelta() * (-1 / view.Zoom);
+            view.Target += mouseDelta;
+        }
+    }
+    static void DrawAxes(ref Camera2D view) 
+    {
+        // Axes position
+        Vector2 axes = Raylib.GetWorldToScreen2D(new Vector2(0, 0), view);
+
+        // Grid
+        Vector2 bottomRight = Raylib.GetScreenToWorld2D(new Vector2(RESOLUTION_X, RESOLUTION_Y), view);
+        Vector2 topLeft = Raylib.GetScreenToWorld2D(new Vector2(0, 0), view);
+        Vector2 nOfLines = new Vector2(bottomRight.X - topLeft.X, bottomRight.Y - topLeft.Y);
+
+        // Vertical
+        int spacingX = (int)(RESOLUTION_X / nOfLines.X);
+        int gridOffsetX = (int)axes.X % spacingX;
+        for (int i = 0; i < nOfLines.X; i++)
+        {
+            int x = i * spacingX + gridOffsetX;
+            Raylib.DrawLine(x, 0, x, RESOLUTION_Y, Color.DarkGray);
+        }
+
+        // Horizontal
+        int spacingY = (int)(RESOLUTION_Y / nOfLines.Y);
+        int gridOffsetY = (int)axes.Y % spacingY;
+        for (int i = 0; i < nOfLines.Y; i++)
+        {
+            int y = i * spacingY + gridOffsetY;
+            Raylib.DrawLine(0, y, RESOLUTION_X, y, Color.DarkGray);
+        }
+
+        // Axes
+        Raylib.DrawLine((int)axes.X, 0, (int)axes.X, RESOLUTION_Y, Color.LightGray);
+        Raylib.DrawLine(0, (int)axes.Y, RESOLUTION_X, (int)axes.Y, Color.LightGray);
+    }
+}
