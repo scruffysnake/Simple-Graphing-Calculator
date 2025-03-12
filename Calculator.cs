e40b8bc1916efddd5d0d19d18fe5921bd77ba040
Scruffysnake <dege.gabriel@gmail.com>
2025-03-12 01:00:15 +0000

    Finished excpet for discontinuity issues

---
+++ Calculator.cs
@@ -0,0 +1,265 @@
+namespace Simple_Graphing_Calculator
+{
+    public enum Operators
+    {
+        PLUS, MINUS, MULTIPLY, DIVIDE, POWER, OPEN, CLOSE, X, ERR, NULL
+    }
+    interface IToken {}
+    struct XVal : IToken {}
+    struct Number : IToken
+    {
+        public double value;
+        public Number(double value) => this.value = value;
+    }
+    struct Operator : IToken
+    {
+        public Operators operatr;
+        public Operator(Operators operatr) => this.operatr = operatr;
+    }
+
+    interface IExpression {}
+    internal class Value : IExpression
+    {
+        public double value;
+        public Value(double value) 
+        { 
+            this.value = value; 
+        }
+    }
+    internal class BinaryExpression : IExpression
+    {
+        public Operators operatr; 
+        public IExpression left, right;
+        public BinaryExpression(Operators operatr, IExpression left, IExpression right) 
+        { 
+            this.operatr = operatr; 
+            this.left = left; 
+            this.right = right; 
+        }
+    }
+    internal class UnaryExpression : IExpression
+    {
+        public Operators operatr; 
+        public IExpression expr;
+        public UnaryExpression(Operators operatr, IExpression expr) 
+        { 
+            this.operatr = operatr; 
+            this.expr = expr; 
+        }
+    }
+    internal class XExpression : IExpression {}
+    internal class BrokenExpression : IExpression {}
+
+    internal class Calculator
+    {
+        public static List<IToken> Tokenise(string input)
+        {
+            List<IToken> tokens = new List<IToken>(); 
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case '\0': case ' ': case '\n': break;
+
+                    case '+': tokens.Add(new Operator(Operators.PLUS)); break;
+                    case '-': tokens.Add(new Operator(Operators.MINUS)); break;
+                    case '*': tokens.Add(new Operator(Operators.MULTIPLY)); break;
+                    case '/': tokens.Add(new Operator(Operators.DIVIDE)); break;
+                    case '^': tokens.Add(new Operator(Operators.POWER)); break;
+                    case '(': tokens.Add(new Operator(Operators.OPEN)); break;
+                    case ')': tokens.Add(new Operator(Operators.CLOSE)); break;
+
+                    case 'X': case 'x': tokens.Add(new XVal()); break;
+
+                    default:
+                        try { tokens.Add(new Number(Number(input, ref i))); }
+                        catch { tokens.Add(new Operator(Operators.ERR)); }
+                        break;
+                }
+            }
+            return tokens;
+        }
+
+        static double Number(string input, ref int i)
+        {
+            Func<char, bool> isNumber = c => char.IsDigit(c);
+            Func<char, bool> isDot = c => c == '.';
+            Func<char, bool> isNumberOrDot = c => isNumber(c) || isDot(c);
+
+            if (!isNumberOrDot(input[i])) throw new Exception("Unexpected char");
+
+            string sNumber = "0";
+            bool hasDot = false;
+            while (i < input.Length && isNumberOrDot(input[i]))
+            {
+                if (isDot(input[i])) 
+                {
+                    if (hasDot) throw new Exception("Can't have multiple decimal places in a number");
+                    hasDot = true;
+                }
+                sNumber += input[i++];
+            }
+            i--;
+
+            return double.Parse(sNumber);
+        }
+    }
+
+    class Parser
+    {
+        List<IToken> tokens;
+        int i;
+        public bool isBroken = false;
+
+        public Parser(List<IToken> tokens) 
+        {
+            this.tokens = tokens;
+            i = 0;
+            if (tokens.Contains(new Operator(Operators.ERR))) isBroken = true;
+        }
+
+        bool match(params Operators[] operators) 
+        {
+            if (i >= tokens.Count) 
+            {
+                isBroken = true;
+                return false;
+            }
+
+            if (tokens[i] is Operator r) 
+            {
+                if (operators.Contains(r.operatr)) 
+                {
+                    i++;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private IExpression parseBinary(Func<IExpression> next, params Operators[] operatorTokens)
+        {
+            IExpression expr = next();
+
+            while (match(operatorTokens))
+            {
+                Operators operatorToken = Operators.NULL;
+                if (tokens[i - 1] is Operator r) 
+                {
+                    operatorToken = r.operatr;
+                }
+                IExpression right = next();
+                expr = new BinaryExpression(operatorToken, expr, right);
+            }
+            return expr;
+        }
+
+        public IExpression parseArithmetic() => parseBinary(parseMultiplicative, Operators.MINUS, Operators.PLUS);
+        private IExpression parseMultiplicative() => parseBinary(parseIndices, Operators.DIVIDE, Operators.MULTIPLY);
+        private IExpression parseIndices() => parseBinary(parseUnary, Operators.POWER);
+
+        private IExpression parseUnary()
+        {
+            if (match(Operators.PLUS, Operators.MINUS))
+            {
+                if (tokens[i - 1] is Operator r) 
+                {
+                    Operators operatorToken = r.operatr;
+                    IExpression expr = parseUnary();
+                    return new UnaryExpression(operatorToken, expr);
+                }
+            }
+            return parsePrimary();
+        }
+
+        private IExpression parsePrimary()
+        {
+            if (i >= tokens.Count) 
+            {
+                isBroken = true;
+                return new BrokenExpression();
+            }
+
+            if (tokens[i] is Number n) 
+            {
+                i++;
+                return new Value(n.value);
+            }
+            if (tokens[i] is XVal) 
+            {
+                i++;
+                return new XExpression();
+            }   
+
+            if (match(Operators.OPEN))
+            {
+                IExpression expr = parseArithmetic();
+                if (i >= tokens.Count)
+                {
+                    isBroken = true;
+                    return new BrokenExpression();
+                }
+                if (tokens[i++] is Operator r) if (r.operatr == Operators.CLOSE) return expr;
+                isBroken = true;
+            }
+            isBroken = true; return new BrokenExpression();
+        }
+    }
+
+    static class Evaluator
+    {
+        public static double x;
+        public static bool error;
+
+        public static void Reset()
+        {
+            error = false;
+        }
+
+        public static double Interpret(IExpression expr)
+        {
+            if (expr is Value val) return val.value;
+            if (expr is BinaryExpression binary) return InterpretBinary(binary);
+            if (expr is UnaryExpression unary) return InterpretUnary(unary);
+            if (expr is XExpression xExpresion) return x;
+            error = true;
+            return 0;
+        }
+        static double InterpretBinary(BinaryExpression binary)
+        {
+            double left = Interpret(binary.left);
+            double right = Interpret(binary.right);
+
+            double BinaryResult(Func<double, double, double> operation) => operation(left, right);
+
+            switch (binary.operatr)
+            {
+                case Operators.PLUS: return BinaryResult((l, r) => l + r);
+                case Operators.MINUS: return BinaryResult((l, r) => l - r);
+                case Operators.MULTIPLY: return BinaryResult((l, r) => l * r);
+                case Operators.DIVIDE: 
+                    if (right == 0) 
+                    {
+                        error = true;
+                        return 0;
+                    }
+                    return BinaryResult((l, r) => l / r);
+                case Operators.POWER: return BinaryResult((l, r) => Math.Pow(l, r));
+            }
+            error = true;
+            return 0;
+        }
+        static double InterpretUnary(UnaryExpression unary)
+        {
+            double val = Interpret(unary.expr);
+            switch (unary.operatr)
+            {
+                case Operators.PLUS: return val;
+                case Operators.MINUS: return -val;
+            }
+            error = true;
+            return 0;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,131 +11,185 @@ using System.Numerics;
     Local variables     :   camelCase
 */
 
-namespace Simple_Graphing_Calculator;
-class Program
+namespace Simple_Graphing_Calculator
 {
-    const int RESOLUTION_X = 1920;
-    const int RESOLUTION_Y = 1080;
-    const float DEFAULT_ZOOM = 1080 / 10;
-
-    static Vector2 Offset = new Vector2(RESOLUTION_X / 2, RESOLUTION_Y / 2);
-
-    public static void Main()
+    class Program
     {
-        // Init
-        Raylib.InitWindow(RESOLUTION_X, RESOLUTION_Y, "Simple Graphing Calculator");
-        Raylib.SetTargetFPS(144);
-        rlImGui.Setup();
+        const int RESOLUTION_X = 1920;
+        const int RESOLUTION_Y = 1080;
+        const float DEFAULT_ZOOM = 1080 / 10;
 
-        // Camera
-        Vector2 targetDefault = new Vector2(0, 0);
-        Camera2D view = new Camera2D(Offset, targetDefault, 0, DEFAULT_ZOOM);
-        Vector2 cameraDefaultPosition = new Vector2(60, 60);
-        Vector2 cameraDefaultSize = new Vector2(300, 80);
+        static Vector2 Offset = new Vector2(RESOLUTION_X / 2, RESOLUTION_Y / 2);
 
-        // Main Loop
-        while (!Raylib.WindowShouldClose())
+        public static void Main()
         {
-            // Input
-            Input(ref view);
+            // Init
+            Raylib.InitWindow(RESOLUTION_X, RESOLUTION_Y, "Simple Graphing Calculator");
+            Raylib.SetTargetFPS(144);
+            rlImGui.Setup();
 
-            // Draw
-            Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.Black);
-                DrawAxes(ref view);
-                DrawMousePosition(ref view);
+            // Camera
+            Vector2 targetDefault = new Vector2(0, 0);
+            Camera2D view = new Camera2D(Offset, targetDefault, 0, DEFAULT_ZOOM);
+            Vector2 cameraDefaultPosition = new Vector2(60, 60);
 
-                // ImGui
-                rlImGui.Begin();
+            // Functions
+            Vector2 functionsDefaultPosition = new Vector2(60, 140);
+            var functions = new List<(string, Vector3)>();
 
-                    // Camera
-                    ImGui.SetNextWindowPos(cameraDefaultPosition, ImGuiCond.FirstUseEver);
-                    ImGui.SetNextWindowSize(cameraDefaultSize, ImGuiCond.FirstUseEver);
-                    ImGui.Begin("Viewport", ImGuiWindowFlags.NoSavedSettings);
-                        ImGui.InputFloat2("Position", ref view.Target);
-                        ImGui.SliderFloat("Zoom", ref view.Zoom, 10, 200);
-                    ImGui.End();
+            // Main Loop
+            while (!Raylib.WindowShouldClose())
+            {
+                // Input
+                Input(ref view);
 
-                    // Functions
-                    ImGui.SetNextWindowPos(new Vector2(60, 160), ImGuiCond.FirstUseEver);
-                    ImGui.Begin("Functions");
-                        ImGui.Text("This is some text");
-                    ImGui.End();
-                rlImGui.End();
-            Raylib.EndDrawing();
+                // Draw
+                Raylib.BeginDrawing();
+                    Raylib.ClearBackground(Color.Black);
+                    DrawAxes(ref view);
+                    DrawFunctions(ref view, functions);
+                    DrawMousePosition(ref view);
+
+                    // ImGui
+                    rlImGui.Begin();
+
+                        // Camera
+                        ImGui.SetNextWindowPos(cameraDefaultPosition, ImGuiCond.FirstUseEver);
+                        ImGui.Begin("Viewport", ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.AlwaysAutoResize);
+                            ImGui.InputFloat2("Position", ref view.Target);
+                            ImGui.SliderFloat("Zoom", ref view.Zoom, 10, 200);
+                        ImGui.End();
+
+                        // Functions
+                        ImGui.SetNextWindowPos(functionsDefaultPosition, ImGuiCond.FirstUseEver);
+                        FunctionsWindow(ref functions);
+
+                    rlImGui.End();
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
         }
-
-        Raylib.CloseWindow();
-    }
-    static void Input(ref Camera2D view)
-    {
-        // Zoom
-        const float ZOOM_INCREMENT = 2.5f;
-        view.Zoom += Raylib.GetMouseWheelMove() * ZOOM_INCREMENT;
-        if (view.Zoom < 10) view.Zoom = 10;
-
-        // Move
-        if (Raylib.IsMouseButtonDown(MouseButton.Right))
+        static void Input(ref Camera2D view)
         {
-            Vector2 mouseDelta = Raylib.GetMouseDelta() * (-1 / view.Zoom);
-            view.Target += mouseDelta;
+            // Zoom
+            const float ZOOM_INCREMENT = 2.5f;
+            view.Zoom += Raylib.GetMouseWheelMove() * ZOOM_INCREMENT;
+            if (view.Zoom < 10) view.Zoom = 10;
+
+            // Move
+            if (Raylib.IsMouseButtonDown(MouseButton.Right))
+            {
+                Vector2 mouseDelta = Raylib.GetMouseDelta() * (-1 / view.Zoom);
+                view.Target += mouseDelta;
+            }
         }
-    }
-    static void DrawAxes(ref Camera2D view) 
-    {
-        // Axes position
-        Vector2 axes = Raylib.GetWorldToScreen2D(new Vector2(0, 0), view);
-
-        // Grid
-        Vector2 bottomRight = Raylib.GetScreenToWorld2D(new Vector2(RESOLUTION_X, RESOLUTION_Y), view);
-        Vector2 topLeft = Raylib.GetScreenToWorld2D(new Vector2(0, 0), view);
-        Vector2 nOfLines = bottomRight - topLeft;
-
-        // Vertical
-        int spacingX = (int)(RESOLUTION_X / nOfLines.X);
-        int gridOffsetX = (int)axes.X % spacingX;
-        for (int i = 0; i < nOfLines.X + 1; i++)
+        static void DrawAxes(ref Camera2D view) 
         {
-            int x = i * spacingX + gridOffsetX;
-            Raylib.DrawLine(x, 0, x, RESOLUTION_Y, Color.DarkGray);
+            // Axes position
+            Vector2 axes = Raylib.GetWorldToScreen2D(new Vector2(0, 0), view);
 
-            // Numbers
-            if (view.Zoom < 75) continue;
-            string positionMarker = Math.Round(Raylib.GetScreenToWorld2D(new Vector2(x, 0), view).X + .1).ToString();
-            if (positionMarker == "0") continue;
-            int markerOffset = (int)Raylib.MeasureTextEx(Raylib.GetFontDefault(), positionMarker, 20, 5).X;
-            Raylib.DrawText(positionMarker, x - markerOffset / 2 - 1, (int)(axes.Y + 5), 20, Color.LightGray);
+            // Grid
+            Vector2 bottomRight = Raylib.GetScreenToWorld2D(new Vector2(RESOLUTION_X, RESOLUTION_Y), view);
+            Vector2 topLeft = Raylib.GetScreenToWorld2D(new Vector2(0, 0), view);
+            Vector2 nOfLines = bottomRight - topLeft;
+
+            // Vertical
+            int spacingX = (int)(RESOLUTION_X / nOfLines.X);
+            int gridOffsetX = (int)axes.X % spacingX;
+            for (int i = 0; i < nOfLines.X + 1; i++)
+            {
+                int x = i * spacingX + gridOffsetX;
+                Raylib.DrawLine(x, 0, x, RESOLUTION_Y, Color.DarkGray);
+
+                // Numbers
+                if (view.Zoom < 75) continue;
+                string positionMarker = Math.Round(Raylib.GetScreenToWorld2D(new Vector2(x, 0), view).X + .1).ToString();
+                if (positionMarker == "0") continue;
+                int markerOffset = (int)Raylib.MeasureTextEx(Raylib.GetFontDefault(), positionMarker, 20, 5).X;
+                Raylib.DrawText(positionMarker, x - markerOffset / 2 - 1, (int)(axes.Y + 5), 20, Color.LightGray);
+            }
+
+            // Horizontal
+            int spacingY = (int)(RESOLUTION_Y / nOfLines.Y);
+            int gridOffsetY = (int)axes.Y % spacingY;
+            for (int i = 0; i < nOfLines.Y + 1; i++)
+            {
+                int y = i * spacingY + gridOffsetY;
+                Raylib.DrawLine(0, y, RESOLUTION_X, y, Color.DarkGray);
+
+                // Numbers
+                if (view.Zoom < 75) continue;
+                string positionMarker = Convert.ToString(Math.Round(Raylib.GetScreenToWorld2D(new Vector2(0, y), view).Y + .1) * -1);
+                if (positionMarker == "-0") continue;
+                int markerOffset = (int)Raylib.MeasureTextEx(Raylib.GetFontDefault(), positionMarker, 20, 5).Y;
+                Raylib.DrawText(positionMarker, (int)(axes.X + 5), y - markerOffset / 2 + 1, 20, Color.LightGray);
+            }
+
+            // Axes
+            Raylib.DrawLine((int)axes.X, 0, (int)axes.X, RESOLUTION_Y, Color.LightGray);
+            Raylib.DrawLine(0, (int)axes.Y, RESOLUTION_X, (int)axes.Y, Color.LightGray);
         }
-
-        // Horizontal
-        int spacingY = (int)(RESOLUTION_Y / nOfLines.Y);
-        int gridOffsetY = (int)axes.Y % spacingY;
-        for (int i = 0; i < nOfLines.Y + 1; i++)
+        static void DrawMousePosition(ref Camera2D view)
         {
-            int y = i * spacingY + gridOffsetY;
-            Raylib.DrawLine(0, y, RESOLUTION_X, y, Color.DarkGray);
+            Vector2 screenPos = Raylib.GetMousePosition();
+            Vector2 worldPos = Raylib.GetScreenToWorld2D(screenPos, view);
 
-            // Numbers
-            if (view.Zoom < 75) continue;
-            string positionMarker = Convert.ToString(Math.Round(Raylib.GetScreenToWorld2D(new Vector2(0, y), view).Y + .1) * -1);
-            if (positionMarker == "-0") continue;
-            int markerOffset = (int)Raylib.MeasureTextEx(Raylib.GetFontDefault(), positionMarker, 20, 5).Y;
-            Raylib.DrawText(positionMarker, (int)(axes.X + 5), y - markerOffset / 2 + 1, 20, Color.LightGray);
+            string coords = $"{Math.Round(worldPos.X, 1)}, {-Math.Round(worldPos.Y, 1)}";
+            int textWidth = Raylib.MeasureText(coords, 30);
+            int x = RESOLUTION_X - 5 - textWidth;
+
+            Raylib.DrawText(coords, x, RESOLUTION_Y - 30, 30, Color.LightGray);
         }
+        static void FunctionsWindow(ref List<(string func, Vector3 colour)> functions)
+        {
+            ImGui.Begin("Functions", ImGuiWindowFlags.NoSavedSettings | ImGuiWindowFlags.AlwaysAutoResize);
+                for (int i = 0; i < functions.Count; i++)
+                {
+                    var currentFunc = functions[i];
+                    ImGui.PushID(i);
+                    ImGui.ColorEdit3("", ref currentFunc.colour, ImGuiColorEditFlags.NoInputs);
+                    ImGui.PopID();
+                    ImGui.SameLine();
+                    ImGui.PushID(i);
+                    ImGui.InputText("= Y", ref currentFunc.func, 128);
+                    ImGui.PopID();
+                    functions[i] = currentFunc;
+                    ImGui.SameLine();
+                    ImGui.PushID(i);
+                    if (ImGui.Button("X"))
+                    {
+                        functions.RemoveAt(i);
+                        i--;
+                    }
+                    ImGui.PopID();
+                }
+                if (ImGui.Button("Add Function")) functions.Add(("", new Vector3(255, 255, 255)));
+            ImGui.End();
+        }
+        static void DrawFunctions(ref Camera2D View, List<(string func, Vector3 colour)> functions)
+        {
+            foreach (var func in functions)
+            {
+                List<IToken> tokens = Calculator.Tokenise(func.func);
+                Parser parser = new Parser(tokens);
+                IExpression parsedFunction = parser.parseArithmetic();
+                for (int i = 0; i < RESOLUTION_X; i++)
+                {
+                    double x = Raylib.GetScreenToWorld2D(new Vector2(i, 0), View).X;
 
-        // Axes
-        Raylib.DrawLine((int)axes.X, 0, (int)axes.X, RESOLUTION_Y, Color.LightGray);
-        Raylib.DrawLine(0, (int)axes.Y, RESOLUTION_X, (int)axes.Y, Color.LightGray);
-    }
-    static void DrawMousePosition(ref Camera2D view)
-    {
-        Vector2 screenPos = Raylib.GetMousePosition();
-        Vector2 worldPos = Raylib.GetScreenToWorld2D(screenPos, view);
+                    Evaluator.Reset();
+                    Evaluator.x = x;
+                    double realY = - Evaluator.Interpret(parsedFunction);
+                    double y = Raylib.GetWorldToScreen2D(new Vector2(0, (float)realY), View).Y;
 
-        string coords = $"{Math.Round(worldPos.X, 1)}, {-Math.Round(worldPos.Y, 1)}";
-        int textWidth = Raylib.MeasureText(coords, 30);
-        int x = RESOLUTION_X - 5 - textWidth;
-
-        Raylib.DrawText(coords, x, RESOLUTION_Y - 30, 30, Color.LightGray);
+                    Color color = new Color(
+                        func.colour.X * 255, 
+                        func.colour.Y * 255, 
+                        func.colour.Z * 255);
+                    Raylib.DrawPixel(i, (int)y, color);
+                }
+            }
+        }
     }
 }
